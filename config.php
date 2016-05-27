346e629f9fe85d765c96520512008b391bf69045
Lgoncede <lgoncede76@gmail.com>
2016-05-27 07:53:13 -0400

    modificando password

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
  $dbhost = "localhost";
  $dbname = "prueba";
  $dbuser = "root";
- $dbpass = "root";
+ $dbpass = "1234";
  
  $con = mysql_connect($dbhost,$dbuser,$dbpass) or die ("problema conectarce a MySqL");
  mysql_select_db ($dbname) or die ("problema al conectarce a la base de datos $dbname");
